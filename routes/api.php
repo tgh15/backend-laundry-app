32265e2ac25c1c7d32f01976eaaa699b0ddba0a4
muh-teguh <muhteguhadhiputra15@gmail.com>
2020-04-22 13:26:14 +0800

    controller and route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function(){
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+    
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('getuser', 'Api\AuthController@getUser');
+    });
+});
