e536eb4e2d6987b215866ec702a22382f9f9f277
muh-teguh <muhteguhadhiputra15@gmail.com>
2020-04-22 20:39:24 +0800

    response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -30,8 +30,9 @@ class AuthController extends Controller
     public function login(){ 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
-            $success['token'] =  $user->createToken('AppName')-> accessToken; 
-            return response()->json(['success' => $success, 'user' => $user], 200); 
+            $success['token'] =  $user->createToken('AppName')-> accessToken;
+            $success['user'] =  $user;
+            return response()->json(['success' => $success], 200); 
         } else{ 
             return response()->json(['error'=>'Unauthorised'], 401); 
         } 
